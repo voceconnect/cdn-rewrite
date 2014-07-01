ca412e58ac7f44957ae5c7d859d42b58e355adfd
Curtis Loisel <curtis@voceconnect.com>
2014-07-01 15:37:03 -0400

    Trying to fix application name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,10 @@
-set :application, 'cdn-rewrite'
-set :repo_url, "git@github.com:voceconnect/#{fetch(:application)}.git"
+set :application, 'wp-cdn-rewrite'
+set :repo_url, "git@github.com:voceconnect/cdn-rewrite.git"
 
 set :scm, 'git-to-svn'
 set :type, 'plugin'
 
-set :svn_repository, "http://plugins.svn.wordpress.org/wp-#{fetch(:application)}/"
+set :svn_repository, "http://plugins.svn.wordpress.org/#{fetch(:application)}/"
 set :svn_deploy_to, "trunk"
 
 set :build_folders, (
